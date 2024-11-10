e713ac097d21d1e3ced20c772f1a3aa418dc9cd4
Grant-E-G <grantget@gmail.com>
2024-11-10 15:10:11 -0800

    fixed plotting error. Copilot doubled mapped when it should have just mapped.

--- src/lesson1.rs
+++ src/lesson1.rs
@@ -1,6 +1,7 @@
 use plotters::prelude::*;
 use clap::Parser;
 
+#[derive(Parser)]
 struct Opts {
     #[clap(long, default_value_t = 100)]
     grid_point_number_x: u32,
@@ -17,7 +18,7 @@ struct Opts {
 }
 
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opts: Opts = Opts::parse();
     let u_lenght = opts.grid_point_number_x as usize; 
     let x_delta = opts.total_x_delta / opts.grid_point_number_x as f32;
@@ -26,6 +27,8 @@ fn main() {
     let lower_bound = (0.5 / x_delta) as usize;
     let upper_bound = (1.0 / x_delta) as usize;
     u_wave_state[lower_bound..upper_bound].fill(2.0);
+    // doesnt seem to fill with 2.0
+    print!("{:?} \n", u_wave_state );
 
     // show the initial state plot
     let root = BitMapBackend::new("wave_initial_state.png", (1024, 768)).into_drawing_area();
@@ -38,7 +41,7 @@ fn main() {
         .build_cartesian_2d(0f32..opts.total_x_delta, 0f32..2.0).unwrap();
     chart.configure_mesh().draw().unwrap();
     chart.draw_series(LineSeries::new(
-        (0..u_lenght).map(|i| i as f32 * x_delta).map(|x| (x, u_wave_state[x as usize])),
+        (0..u_lenght).map(|i| (i as f32 * x_delta, u_wave_state[i])),
         &RED,
     ))?
     .label("Initial State")
