ce0a425a1b5b2215242b7dc18f3512660427108e
Grant-E-G <grantget@gmail.com>
2024-11-10 14:57:31 -0800

    plot inital state setup.

--- src/lesson1.rs
+++ src/lesson1.rs
@@ -23,6 +23,32 @@ fn main() {
     let x_delta = opts.total_x_delta / opts.grid_point_number_x as f32;
     let mut u_wave_state = vec![1.0; u_lenght];
     // We are using inital conditions u is 2.0 for 0.5 <= x <= 1.0, and 1.0 otherwise
-    let lower_bound
+    let lower_bound = (0.5 / x_delta) as usize;
+    let upper_bound = (1.0 / x_delta) as usize;
+    u_wave_state[lower_bound..upper_bound].fill(2.0);
+
+    // show the initial state plot
+    let root = BitMapBackend::new("wave_initial_state.png", (1024, 768)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+    let mut chart = ChartBuilder::on(&root)
+        .caption("Wave Equation", ("sans-serif", 50).into_font())
+        .margin(5)
+        .x_label_area_size(40)
+        .y_label_area_size(40)
+        .build_cartesian_2d(0f32..opts.total_x_delta, 0f32..2.0).unwrap();
+    chart.configure_mesh().draw().unwrap();
+    chart.draw_series(LineSeries::new(
+        (0..u_lenght).map(|i| i as f32 * x_delta).map(|x| (x, u_wave_state[x as usize])),
+        &RED,
+    ))?
+    .label("Initial State")
+    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    chart.configure_series_labels().background_style(&WHITE.mix(0.8)).border_style(&BLACK).draw().unwrap();
+
+    root.present().unwrap();
+
+    Ok(())
+
+
     
 }
